b0cc6ee5f4b167b4c959680950923d0fd5de0da3
Ashish Pandey <email@address.com>
2020-11-24 21:23:26 +0530

    Commit now

---
+++ qa/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  qa
+//
+//  Created by Ashish Pandey on 24/11/20.
+//  Copyright © 2020 Simmy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("sdfdsf")
+    }
+
+
+}
+
